19ac64dafa205cabd71d1cd27dd40f30ef8c63a8
Gary Tsai <Gary_Tsai@PFT.com>
2019-06-28 16:34:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            load: false,
+            errorMessage:''
+        }
+    }
+    create =(e)=>
+    {
+
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({video: true})
+                .then(function (stream) {
+                    this.video.srcObject = stream;
+                    this.setState({
+                        videoStream: stream.active
+                    })
+                })
+                .catch(function (err0r) {
+                    console.log(err0r);
+                    console.log("Something went wrong!");
+                });
+        }
+    }
+    release =(e)=> {
+        let stream = this.video.srcObject;
+        let tracks = stream.getTracks();
+
+        for (let i = 0; i < tracks.length; i++) {
+            let track = tracks[i];
+            track.stop();
+        }
+        this.video.srcObject = null;
+    }
+    render() {
+      const container =  {
+        margin: '0px auto',
+        width: '800px',
+        height: '450px',
+        textAlign: 'center'
+    }
+      const videoElement =  {
+          display: 'block',
+        width: '800px',
+      height: '450px',
+      backgroundColor: '#666'
+    }
+
+        const btn_style={
+            width: '150px'
+        }
+
+
+        return (
+            <div>
+              <div style={container}>
+
+                <video autoPlay={true} ref={video => this.video =video} id="videoElement"style={videoElement}>
+
+                </video>
+                  <div>
+                  { !this.state.videoStream  &&
+                  <button type='button'  style={btn_style} onClick={event => this.create(event)}>create stream</button>}
+                  { this.state.videoStream &&
+                  <button type='button' style={btn_style} onClick={event => this.release(event)}>release stream</button>}
+                  </div>
+
+              </div>
+                <div>{this.state.videoStream.toString() }123 </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
